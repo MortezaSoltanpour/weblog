ecce1d32001deb953fc56e8a59c01c7ea202a77c
morTeza solTanpour <morteza.cs@live.com>
2022-08-16 19:09:48 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const dotEnv = require("dotenv");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+const blogRoutes = require("./routes/blog");
+const dashRoutes = require("./routes/dashboard");
+
+// Load Config
+dotEnv.config({ path: "./config/config.env" });
+connectDB();
+
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+// View engine
+app.use(expressLayout);
+app.set("layout", "./Layouts/mainLayout");
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/dashboard", dashRoutes);
+app.use(blogRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
---
+++ routes/dashboard.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const router = new Router();
+
+//  @desc   Dashboard
+//  @route  GET /dashboard
+router.get("/", (req, res) => {
+  res.render("dashboard", {
+    pageTitle: "بخش مدیریت | داشبورد",
+    path: "/dashboard",
+    layout: "./layouts/dashLayout",
+  });
+});
+
+//  @desc   Login Page
+//  @route  GET /dashboard/login
+router.get("/login", (req, res) => {
+  res.render("login", { pageTitle: "ورود به بخش مدیریت", path: "/login" });
+});
+
+module.exports = router;
