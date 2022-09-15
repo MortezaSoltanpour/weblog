f74c612b94a4e13db3823439b03e9640916cd6d6
morTeza solTanpour <morteza.cs@live.com>
2022-09-15 15:41:06 +0430

    Sep 15th 2022

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,10 @@
+const multer = require("multer");
+
 const Blog = require("../models/Blog");
 const { formatDate } = require("../utils/jalali");
 
 const { get500 } = require("./errorController");
+const { storage, fileFilter } = require("../utils/multer");
 
 exports.getDashboard = async (req, res) => {
   try {
@@ -30,11 +33,48 @@ exports.getAddPost = (req, res) => {
 };
 
 exports.createPost = async (req, res) => {
+  const errorArr = [];
+
   try {
+    await Blog.postValidation(req.body);
     await Blog.create({ ...req.body, user: req.user.id });
-  } catch (error) {
-    console.log(error);
-    get500(req, res);
+  } catch (err) {
+    console.log(err);
+    //get500(req, res);
+    err.inner.forEach((e) => {
+      errorArr.push({
+        name: e.path,
+        message: e.message,
+      });
+    });
+    res.render("private/addPost", {
+      pageTitle: "بخش مدیریت | ساخت پست جدید",
+      path: "/dashboard/add-post",
+      layout: "./layouts/dashLayout",
+      fullname: req.user.fullname,
+      errors: errorArr,
+    });
   }
   res.redirect("/dashboard");
+};
+
+exports.uploadImage = (req, res) => {
+  const upload = multer({
+    limits: { fileSize: 4000000 },
+    dest: "uploads/",
+    storage: storage,
+    fileFilter: fileFilter,
+  }).single("image");
+
+  upload(req, res, (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      if (req.file) {
+        res.status(200).send("آپلود عکس موفقیت آمیز بود");
+      } else {
+        res.send("جهت آپلود باید عکسی انتخاب کنید");
+      }
+    }
+  });
 };
--- models/Blog.js
+++ models/Blog.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+const { schema } = require("./secure/postvalidation");
+
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
     trim: true,
     minLength: 5,
-    maxLength: 255,
+    maxLength: 100,
   },
   body: {
     type: String,
@@ -26,6 +28,10 @@ const blogSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+blogSchema.statics.postValidation = function (body) {
+  return schema.validate(body, { abortEarly: false });
+};
 
 const Blog = mongoose.model("Blog", blogSchema);
 
